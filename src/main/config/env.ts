e1c2c36666917a4bd32c30f71072c661ff7663c7
Brendo Vale <brendo.vale02@gmail.com>
2021-01-06 17:05:20 -0300

    refactor: update docker and config env

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,5 +1,5 @@
 export default {
   mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:27017/artistsalbuns',
-  port: process.env.PORT || 1234,
+  port: process.env.PORT || 3000,
   jwtSecret: process.env.JWT_SECRET || 'vlI0aHfnFp28kIxkJS_DpraYWhCfEycR3T-T7Fr8PLzn1VhsbyxjNSBVcy77zl6hF_5qcOF9Re7Acb9rg7XnLzZkqhvsMUmzsvcmnotc30xtecifl2PgNpFqPKrd1sH-6HWF5KtlgVabFVAmNQm24BjEYIU3RlxJa9or6UXxQ6s'
 }
