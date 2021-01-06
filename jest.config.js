fe3d425aa03936f1e7b31e60c6f7dceb7cc4dd40
Brendo Vale <brendo.vale02@gmail.com>
2021-01-05 21:44:03 -0400

    chore: add module-alias js

--- jest.config.js
+++ jest.config.js
@@ -17,5 +17,8 @@ module.exports = {
   // A map from regular expressions to paths to transformers
   transform: {
     '.+\\.ts$': 'ts-jest'
+  },
+  moduleNameMapper: {
+    '@/(.*)': '<rootDir>/src/$1'
   }
 }
