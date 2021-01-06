8408026b7e89d18b62a043ecfc3456b6c6a041ad
Brendo Vale <brendo.vale02@gmail.com>
2021-01-05 23:18:06 -0400

    feat: ensure AuthenticationMongoRepository return an account on LoadByEmail success

--- src/infra/db/mongodb/authenticate/authenticate-repository.ts
+++ src/infra/db/mongodb/authenticate/authenticate-repository.ts
@@ -1,13 +1,20 @@
 import { AddAccountRepository } from '@/data/protocols/db/authentication/add-account-repository'
+import { LoadAccountByEmailRepository } from '@/data/protocols/db/authentication/load-account-by-email-repository'
 import { UserModel } from '@/domain/models'
 import { AddAccountModel } from '@/domain/usecases'
 import { MongoHelper } from '../helpers/mongo-helpers'
 
-export class AuthenticateMongoRepository implements AddAccountRepository {
+export class AuthenticateMongoRepository implements AddAccountRepository, LoadAccountByEmailRepository {
   private readonly collection: string = 'users'
   async add(accountData: AddAccountModel): Promise<UserModel> {
     const accountCollection = await MongoHelper.getCollection(this.collection)
     const account = await accountCollection.insertOne(accountData)
     return MongoHelper.mapper(account.ops[0])
+  }
+
+  async loadByEmail(email: string): Promise<UserModel> {
+    const accountCollection = await MongoHelper.getCollection(this.collection)
+    const account = await accountCollection.findOne({ email })
+    return account && MongoHelper.mapper(account)
   }
 }
