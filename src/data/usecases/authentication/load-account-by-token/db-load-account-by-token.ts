58bbe8d59f7a3fb97f4ece50b769a919049d61e4
Brendo Vale <brendo.vale02@gmail.com>
2021-01-06 03:21:46 -0400

    feat: ensure DbLoadAccountByToken return null if LoadAccountByTokenRepository retuns null

--- src/data/usecases/authentication/load-account-by-token/db-load-account-by-token.ts
+++ src/data/usecases/authentication/load-account-by-token/db-load-account-by-token.ts
@@ -1,7 +1,6 @@
 import { LoadAccountByToken } from '@/domain/usecases/authentication/load-account-by-token'
 import { Decrypter } from '@/data/protocols/criptography/dencrypter'
 import { UserModel } from '@/domain/models'
-import { mockAccountModel } from '@/data/test'
 import { LoadAccountByTokenRepository } from '@/data/protocols/db/authentication/load-account-by-token-repository'
 
 export class DbLoadAccountByToken implements LoadAccountByToken {
@@ -13,8 +12,10 @@ export class DbLoadAccountByToken implements LoadAccountByToken {
   async loadByToken(accessToken: string, role?: string): Promise<UserModel> {
     const token = await this.decrypter.decrypt(accessToken)
     if (token) {
-      await this.loadAccountByTokenRepository.loadByToken(accessToken, role)
-      return mockAccountModel()
+      const account = await this.loadAccountByTokenRepository.loadByToken(accessToken, role)
+      if (account) {
+        return account
+      }
     }
     return null
   }
