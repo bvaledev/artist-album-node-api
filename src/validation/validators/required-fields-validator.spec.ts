62a2859cd382983571a2baa502b620f35444f397
Brendo Vale <brendo.vale02@gmail.com>
2021-01-05 23:55:26 -0400

    test: ensure Required Field Validator not return if validation success

--- src/validation/validators/required-fields-validator.spec.ts
+++ src/validation/validators/required-fields-validator.spec.ts
@@ -11,4 +11,10 @@ describe('RequiredField Validation', () => {
     const error = sut.validate({ email: 'any_email@email.com' })
     expect(error).toEqual(new MissingParamError('any_field'))
   })
+
+  test('Should not return if validation success', () => {
+    const sut = makeSut()
+    const error = sut.validate({ any_field: 'any_field' })
+    expect(error).toBeFalsy()
+  })
 })
