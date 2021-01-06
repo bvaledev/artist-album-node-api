8ced828b52ac4cf8e158b7249b2af9f3fd01872d
Brendo Vale <brendo.vale02@gmail.com>
2021-01-06 04:21:54 -0400

    test: ensure JwtAdapter return a token on sign success

--- src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
+++ src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
@@ -22,6 +22,12 @@ describe('JWT Adapter', () => {
       await sut.encrypt('any_id')
       expect(signSpy).toHaveBeenCalledWith({ id: 'any_id' }, 'secret')
     })
+
+    test('Should return a token on sign success', async () => {
+      const sut = makeSut()
+      const accessToken = await sut.encrypt('any_id')
+      expect(accessToken).toBe('any_token')
+    })
   })
 
   describe('verify()', () => {
