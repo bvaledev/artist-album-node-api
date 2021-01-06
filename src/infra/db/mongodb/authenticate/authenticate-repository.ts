bb37e53c46b42318845972fd9294d33cd28f182d
Brendo Vale <brendo.vale02@gmail.com>
2021-01-05 23:12:50 -0400

    feat: ensure AuthenticationMongoRepository return an account on add success

--- src/infra/db/mongodb/authenticate/authenticate-repository.ts
+++ src/infra/db/mongodb/authenticate/authenticate-repository.ts
@@ -1,9 +1,13 @@
 import { AddAccountRepository } from '@/data/protocols/db/authentication/add-account-repository'
 import { UserModel } from '@/domain/models'
 import { AddAccountModel } from '@/domain/usecases'
+import { MongoHelper } from '../helpers/mongo-helpers'
 
 export class AuthenticateMongoRepository implements AddAccountRepository {
+  private readonly collection: string = 'users'
   async add(accountData: AddAccountModel): Promise<UserModel> {
-    throw new Error('Method not implemented.')
+    const accountCollection = await MongoHelper.getCollection(this.collection)
+    const account = await accountCollection.insertOne(accountData)
+    return MongoHelper.mapper(account.ops[0])
   }
 }
