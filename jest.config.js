4724ca9cbed36be67d8410c6a75eace53dfe381e
Brendo Vale <brendo.vale02@gmail.com>
2021-01-05 20:23:18 -0400

    chore: add jest config

---
+++ jest.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  // tsconfigRootDir: __dirname,
+  roots: ['<rootDir>/src'],
+  // An array of glob patterns indicating a set of files for which coverage information should be collected
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts',
+    '!<rootDir>/src/**/index.ts',
+    '!<rootDir>/src/main/**'
+  ],
+  preset: '@shelf/jest-mongodb',
+  // The directory where Jest should output its coverage files
+  coverageDirectory: 'coverage',
+  // Indicates which provider should be used to instrument code for coverage
+  coverageProvider: 'babel',
+  // The test environment that will be used for testing
+  testEnvironment: 'node',
+  // A map from regular expressions to paths to transformers
+  transform: {
+    '.+\\.ts$': 'ts-jest'
+  }
+}
