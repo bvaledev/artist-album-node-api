c04c3da6aa78ce1d3934f8c9faf4fc5a39e37c67
Brendo Vale <brendo.vale02@gmail.com>
2021-01-06 05:22:17 -0400

    feat: ensure login route return 401 on login fail

--- src/main/routes/authentication-routes.test.ts
+++ src/main/routes/authentication-routes.test.ts
@@ -47,5 +47,14 @@ describe('Login Routes', () => {
         })
         .expect(200)
     })
+
+    test('Should return 401 on login fail', async () => {
+      await request(app).post('/api/login')
+        .send({
+          email: 'any_email@email.com',
+          password: '123'
+        })
+        .expect(401)
+    })
   })
 })
