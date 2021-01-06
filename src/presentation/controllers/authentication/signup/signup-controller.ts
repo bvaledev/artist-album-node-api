4086446f40c2c2cc336232cbdc99394f38aa0b4e
Brendo Vale <brendo.vale02@gmail.com>
2021-01-05 23:49:27 -0400

    feat: ensure SignupController return 400 if an required fields not provided

--- src/presentation/controllers/authentication/signup/signup-controller.ts
+++ src/presentation/controllers/authentication/signup/signup-controller.ts
@@ -1,5 +1,6 @@
 import { AddAccount } from '@/domain/usecases'
-import { ok, serverError } from '@/presentation/helpers'
+import { EmailInUseError, MissingParamError } from '@/presentation/errors'
+import { badRequest, forbidden, ok, serverError } from '@/presentation/helpers'
 import { Controller, HttpRequest, HttpResponse } from '@/presentation/protocols'
 
 export class SignUpController implements Controller {
@@ -9,12 +10,21 @@ export class SignUpController implements Controller {
 
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
+      const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
+      for (const field of requiredFields) {
+        if (!httpRequest.body[field]) {
+          return badRequest(new MissingParamError(field))
+        }
+      }
       const { name, email, password } = httpRequest.body
       const account = await this.addAccount.add({
         name,
         email,
         password
       })
+      if (!account) {
+        return forbidden(new EmailInUseError())
+      }
       return ok(account)
     } catch (error) {
       return serverError(error)
