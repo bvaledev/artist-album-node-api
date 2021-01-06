b02cf04be834b94d2e9f222748e9b91809aa3721
Brendo Vale <brendo.vale02@gmail.com>
2021-01-06 01:07:48 -0400

    feat: add server config

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,1 +1,10 @@
+import { MongoHelper } from '@/infra/db/mongodb/helpers/mongo-helpers'
 import 'module-alias/register'
+import env from './config/env'
+
+MongoHelper.connect(env.mongoUrl).then(async () => {
+  const app = (await import('./config/app')).default
+  app.listen(env.port, () => {
+    console.log(`server runnint at http://localhost:${env.port}`)
+  })
+})
