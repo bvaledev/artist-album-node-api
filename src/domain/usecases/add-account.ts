986117254cb5b03fe4702fe63d7022e4282a7adb
Brendo Vale <brendo.vale02@gmail.com>
2021-01-05 22:06:46 -0400

    feat: add domain usecase add-account

---
+++ src/domain/usecases/add-account.ts
@@ -0,0 +1,11 @@
+import { UserModel } from '@/domain/models'
+
+export type AddAccountModel = {
+  name: string
+  email: string
+  password: string
+}
+
+export interface AddAccount {
+  add: (account: AddAccountModel) => Promise<UserModel>
+}
