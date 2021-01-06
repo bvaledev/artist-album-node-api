99d33ba9901eb9d7a299e962d5edc2df8442e956
Brendo Vale <brendo.vale02@gmail.com>
2021-01-06 03:55:51 -0400

    test: ensure DbAuthentication return null if HashCompare returns false

--- src/data/usecases/authentication/authenticate/db-authentication.spec.ts
+++ src/data/usecases/authentication/authenticate/db-authentication.spec.ts
@@ -76,4 +76,20 @@ describe('DbAuthentication UseCase', () => {
     await sut.auth(makeFakeAuthentication())
     expect(compareSpy).toHaveBeenCalledWith('any_password', 'hashed_password')
   })
+
+  test('Should throw if HashCompare throws', async () => {
+    const { sut, hashCompareStub } = makeSut()
+    jest.spyOn(hashCompareStub, 'compare').mockImplementationOnce((): never => {
+      throw new Error()
+    })
+    const promise = sut.auth(makeFakeAuthentication())
+    await expect(promise).rejects.toThrow()
+  })
+
+  test('Should return null if HashCompare returns false', async () => {
+    const { sut, hashCompareStub } = makeSut()
+    jest.spyOn(hashCompareStub, 'compare').mockReturnValueOnce(new Promise((resolve) => resolve(false)))
+    const accessToken = await sut.auth(makeFakeAuthentication())
+    expect(accessToken).toBe(null)
+  })
 })
