7687472857219d919610244b830ca8e1cf4e6626
Brendo Vale <brendo.vale02@gmail.com>
2021-01-06 03:31:07 -0400

    feat: ensure AuthenticateMongoRepository return an account on loadByToken success without role

--- src/infra/db/mongodb/authenticate/authenticate-repository.ts
+++ src/infra/db/mongodb/authenticate/authenticate-repository.ts
@@ -2,9 +2,10 @@ import { AddAccountRepository } from '@/data/protocols/db/authentication/add-acc
 import { LoadAccountByEmailRepository } from '@/data/protocols/db/authentication/load-account-by-email-repository'
 import { UserModel } from '@/domain/models'
 import { AddAccountModel } from '@/domain/usecases'
+import { LoadAccountByToken } from '@/domain/usecases/authentication/load-account-by-token'
 import { MongoHelper } from '../helpers/mongo-helpers'
 
-export class AuthenticateMongoRepository implements AddAccountRepository, LoadAccountByEmailRepository {
+export class AuthenticateMongoRepository implements AddAccountRepository, LoadAccountByEmailRepository, LoadAccountByToken {
   private readonly collection: string = 'users'
   async add(accountData: AddAccountModel): Promise<UserModel> {
     const accountCollection = await MongoHelper.getCollection(this.collection)
@@ -15,6 +16,12 @@ export class AuthenticateMongoRepository implements AddAccountRepository, LoadAc
   async loadByEmail(email: string): Promise<UserModel> {
     const accountCollection = await MongoHelper.getCollection(this.collection)
     const account = await accountCollection.findOne({ email })
+    return account && MongoHelper.mapper(account)
+  }
+
+  async loadByToken(accessToken: string, role?: string): Promise<UserModel> {
+    const accountCollection = await MongoHelper.getCollection(this.collection)
+    const account = await accountCollection.findOne({ accessToken, $or: [{ role }, { role: 'admin' }] })
     return account && MongoHelper.mapper(account)
   }
 }
