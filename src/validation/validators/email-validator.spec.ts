eb05762993f6f7d3b6fc85f69677089456f6eb3f
Brendo Vale <brendo.vale02@gmail.com>
2021-01-06 00:06:03 -0400

    test: ensure EmailValidator not return error if no email is provided

--- src/validation/validators/email-validator.spec.ts
+++ src/validation/validators/email-validator.spec.ts
@@ -9,4 +9,10 @@ describe('EmailValidator', () => {
     const error = sut.validate({ email: 'invalid_email@' })
     expect(error).toEqual(new InvalidParamError('email'))
   })
+
+  test('Should not return error if no email is provided', () => {
+    const sut = makeSut()
+    const error = sut.validate({ password: 'any_password', passwordConfirmation: 'any_password' })
+    expect(error).toBeFalsy()
+  })
 })
