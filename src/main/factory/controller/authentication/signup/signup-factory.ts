d954e08d94ac35a49e6ae4a15ca970e8d997a2e2
Brendo Vale <brendo.vale02@gmail.com>
2021-01-06 00:41:57 -0400

    feat: add makeSignUpController factory

---
+++ src/main/factory/controller/authentication/signup/signup-factory.ts
@@ -0,0 +1,8 @@
+import { makeDbAddAccountFactory } from '@/main/factory/usecases/make-db-add-account-factory'
+import { SignUpController } from '@/presentation/controllers/authentication/signup/signup-controller'
+import { Controller } from '@/presentation/protocols'
+import { makeSignupValidationComposite } from './signup-validation'
+
+export const makeSignUpControllerFactory = (): Controller => {
+  return new SignUpController(makeSignupValidationComposite(), makeDbAddAccountFactory())
+}
