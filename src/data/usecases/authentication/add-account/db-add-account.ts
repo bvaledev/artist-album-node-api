04b5be2d2a015d41146d4c55518c959bf0d2aa7a
Brendo Vale <brendo.vale02@gmail.com>
2021-01-05 22:37:21 -0400

    feat: ensure DbAddAccount calls Hasher with correct values

--- src/data/usecases/authentication/add-account/db-add-account.ts
+++ src/data/usecases/authentication/add-account/db-add-account.ts
@@ -1,13 +1,16 @@
+import { Hasher } from '@/data/protocols/criptography/hasher'
 import { AddAccountRepository } from '@/data/protocols/db/authentication/add-account-repository'
 import { UserModel } from '@/domain/models'
 import { AddAccount, AddAccountModel } from '@/domain/usecases'
 
 export class DbAddAccount implements AddAccount {
   constructor(
+    protected readonly hash: Hasher,
     protected readonly addAccountRepo: AddAccountRepository
   ) { }
 
   async add(accountData: AddAccountModel): Promise<UserModel> {
+    await this.hash.hash(accountData.password)
     const account = this.addAccountRepo.add(accountData)
     return account
   }
