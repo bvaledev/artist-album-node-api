c9e846f73d8c9af0539f8e56380bce231c56c89e
Brendo Vale <brendo.vale02@gmail.com>
2021-01-05 22:27:56 -0400

    test: ensure DbAuthentication call AddAccountRepository with correct values

--- src/data/usecases/authentication/add-account/db-add-account.spec.ts
+++ src/data/usecases/authentication/add-account/db-add-account.spec.ts
@@ -4,19 +4,29 @@ import { DbAddAccount } from './db-add-account'
 
 interface SutTypes {
   sut: DbAddAccount
-  addAccountRepo: AddAccountRepository
+  addAccountRepoStub: AddAccountRepository
 }
 
 const makeSut = (): SutTypes => {
-  const addAccountRepo = mockAddAccountRepository()
-  const sut = new DbAddAccount(addAccountRepo)
+  const addAccountRepoStub = mockAddAccountRepository()
+  const sut = new DbAddAccount(addAccountRepoStub)
   return {
     sut,
-    addAccountRepo
+    addAccountRepoStub
   }
 }
 
 describe('DbAddAccount UseCase', () => {
+  test('Should call AddAccountRepository with correct values', async () => {
+    const { sut, addAccountRepoStub } = makeSut()
+    const addSpy = jest.spyOn(addAccountRepoStub, 'add')
+    await sut.add(mockAddAccountParams())
+    expect(addSpy).toHaveBeenCalledWith({
+      name: 'any_name',
+      email: 'any_email@email.com',
+      password: 'any_password'
+    })
+  })
   test('Should return an accont on sucess', async () => {
     const { sut } = makeSut()
     const addAccountRepository = await sut.add(mockAddAccountParams())
