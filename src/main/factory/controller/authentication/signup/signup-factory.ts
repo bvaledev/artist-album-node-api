4675dea0e7c436a0757102027100df04b403bd20
Brendo Vale <brendo.vale02@gmail.com>
2021-01-06 05:22:00 -0400

    feat: ensure login route return 200 on login

--- src/main/factory/controller/authentication/signup/signup-factory.ts
+++ src/main/factory/controller/authentication/signup/signup-factory.ts
@@ -1,8 +1,9 @@
 import { makeDbAddAccountFactory } from '@/main/factory/usecases/make-db-add-account-factory'
+import { makeDbAuthenticationFactory } from '@/main/factory/usecases/make-db-authentication-factory'
 import { SignUpController } from '@/presentation/controllers/authentication/signup/signup-controller'
 import { Controller } from '@/presentation/protocols'
 import { makeSignupValidationComposite } from './signup-validation'
 
 export const makeSignUpControllerFactory = (): Controller => {
-  return new SignUpController(makeSignupValidationComposite(), makeDbAddAccountFactory())
+  return new SignUpController(makeSignupValidationComposite(), makeDbAddAccountFactory(), makeDbAuthenticationFactory())
 }
--- src/main/routes/authentication-routes.test.ts
+++ src/main/routes/authentication-routes.test.ts
@@ -2,6 +2,7 @@ import request from 'supertest'
 import app from '@/main/config/app'
 import { MongoHelper } from '@/infra/db/mongodb/helpers/mongo-helpers'
 import { Collection } from 'mongodb'
+import { hash } from 'bcrypt'
 
 let authCollection: Collection
 
@@ -15,7 +16,7 @@ describe('Login Routes', () => {
   })
 
   beforeEach(async () => {
-    authCollection = await MongoHelper.getCollection('accounts')
+    authCollection = await MongoHelper.getCollection('users')
     await authCollection.deleteMany({})
   })
 
@@ -28,6 +29,23 @@ describe('Login Routes', () => {
           password: '123456789',
           passwordConfirmation: '123456789'
         }).expect(200)
+    })
+  })
+
+  describe('POST /login', () => {
+    test('Should return 200 on login', async () => {
+      const passwordhash = await hash('123456789', 12)
+      await authCollection.insertOne({
+        name: 'Brendo',
+        email: 'brendo@brendo.me',
+        password: passwordhash
+      })
+      await request(app).post('/api/login')
+        .send({
+          email: 'brendo@brendo.me',
+          password: '123456789'
+        })
+        .expect(200)
     })
   })
 })
