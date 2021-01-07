af70ead4df6f104dab728833fe55d26995292d42
Brendo Vale <brendo.vale02@gmail.com>
2021-01-07 16:21:00 -0300

    feat: ensure  DbAddArtist call LoadArtistByNameRepository with correct value

--- src/data/usecases/artist/add-artist/db-add-artist.ts
+++ src/data/usecases/artist/add-artist/db-add-artist.ts
@@ -1,11 +1,21 @@
 import { AddArtistRepository } from '@/data/protocols/db/artist/add-artist-repository'
+import { LoadArtistByNameRepository } from '@/data/protocols/db/artist/load-artist-by-name-repository'
 import { ArtistModel } from '@/domain/models'
 import { AddArtist, AddArtistModel } from '@/domain/usecases/artists/add-artist'
 
 export class DbAddArtist implements AddArtist {
-    constructor(private readonly addArtistRepository: AddArtistRepository) {}
+    constructor(
+        private readonly addArtistRepository: AddArtistRepository,
+        private readonly loadArtistByNameRepository: LoadArtistByNameRepository
+    ) {}
     async add(artist: AddArtistModel): Promise<ArtistModel> {
-        const data = this.addArtistRepository.add(artist)
-        return data
+        const artistExists = await this.loadArtistByNameRepository.loadByName(artist.name)
+
+        if(!artistExists){
+            const data = this.addArtistRepository.add(artist)
+            return data
+        }
+
+        return null
     }
 }
