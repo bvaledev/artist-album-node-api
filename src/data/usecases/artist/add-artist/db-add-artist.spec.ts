d014e4e374a4595a9cbc048a9e82b72175af2046
Brendo Vale <brendo.vale02@gmail.com>
2021-01-07 10:45:16 -0300

    test: ensure DbAddArtist call AddArtistRepository with correct value

---
+++ src/data/usecases/artist/add-artist/db-add-artist.spec.ts
@@ -0,0 +1,43 @@
+import { AddArtistRepository } from "@/data/protocols/db/artist/add-artist-repository"
+import { ArtistModel } from "@/domain/models"
+import { AddArtistModel } from "@/domain/usecases/artists/add-artist"
+import { DbAddArtist } from "./db-add-artist"
+
+const mockArtistModel = ():ArtistModel => ({
+    id: 'any_id',
+    name: 'any_name'
+})
+
+const mockAddArtistRepository = (): AddArtistRepository => {
+    class AddArtistRepositoryStub implements AddArtistRepository {
+        async add(artist: AddArtistModel): Promise<ArtistModel> {
+            return Promise.resolve(mockArtistModel())
+        }
+    }
+    return new AddArtistRepositoryStub()
+}
+
+type SutType = {
+    sut: AddArtistRepository,
+    addArtistRepositoryStub: AddArtistRepository
+}
+
+const makeSut = (): SutType => {
+    const addArtistRepositoryStub = mockAddArtistRepository()
+    const sut = new DbAddArtist(addArtistRepositoryStub)
+
+    return {
+        addArtistRepositoryStub,
+        sut
+    }
+}
+
+describe('DbAddArtist UseCase', () => {
+    test('Should call AddArtistRepository with correct value', async () => {
+        const { sut, addArtistRepositoryStub } = makeSut()
+        const mockAddArtist = {
+            name: 'any_name'
+        }
+        await sut.add(mockAddArtist)
+    })
+})
