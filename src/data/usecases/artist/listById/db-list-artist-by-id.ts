4bc1f1a9d4d7ae48940df5aaec50183762107438
Brendo Vale <brendo.vale02@gmail.com>
2021-01-07 16:02:11 -0300

    refactor: update dependencies import

--- src/data/usecases/artist/listById/db-list-artist-by-id.ts
+++ src/data/usecases/artist/listById/db-list-artist-by-id.ts
@@ -1,8 +1,9 @@
+import { LoadArtistByIdRepository } from '@/data/protocols/db/artist/load-artist-by-id-repository'
 import { ArtistModel } from '@/domain/models'
 import { LoadArtistById } from '@/domain/usecases/artists/load-artist-by-id'
 
 export class DbLoadArtistById implements LoadArtistById {
-    constructor(private readonly loadArtistByIdRepository: LoadArtistById) {}
+    constructor(private readonly loadArtistByIdRepository: LoadArtistByIdRepository) {}
     async loadById(id: string): Promise<ArtistModel> {
         return await this.loadArtistByIdRepository.loadById(id)
     }
--- src/data/usecases/artist/listByName/db-list-artist-by-name.ts
+++ src/data/usecases/artist/listByName/db-list-artist-by-name.ts
@@ -1,8 +1,9 @@
+import { LoadArtistByNameRepository } from '@/data/protocols/db/artist/load-artist-by-name-repository'
 import { ArtistModel } from '@/domain/models'
 import { LoadArtistByName } from '@/domain/usecases/artists/load-artist-by-name'
 
 export class DbLoadArtistByName implements LoadArtistByName {
-    constructor(private readonly loadArtistByNameRepository: LoadArtistByName) {}
+    constructor(private readonly loadArtistByNameRepository: LoadArtistByNameRepository) {}
     async loadByName(name: string): Promise<ArtistModel> {
         return await this.loadArtistByNameRepository.loadByName(name)
     }
