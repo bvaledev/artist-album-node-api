01abbd87edf576aabbf93794f8dc4ac4c4bb5653
Brendo Vale <brendo.vale02@gmail.com>
2021-01-07 22:03:17 -0400

    test: ensure DbAddAlbum call LoadAlbumByNameRepository with correct value

---
+++ src/data/usecases/album/add-artist/db-add-album.spec.ts
@@ -0,0 +1,67 @@
+import { AddAlbumRepository } from "@/data/protocols/db/album/add-albums-repository"
+import { LoadAlbumByNameRepository } from "@/data/protocols/db/album/load-albums-by-name-repository"
+import { AlbumModel } from "@/domain/models"
+import { AddAlbumModel } from "@/domain/usecases/album"
+import { DbAddAlbum } from "./db-add-album"
+
+const mockAlbumModel = (name: string): AlbumModel => ({
+    id: 'any_id',
+    artist_id: 'any_id_red',
+    name: name,
+    year: '2000',
+    youtube: 'http://youtube.com',
+    images: [' image_1', 'image_2', 'image_3', 'image_4']
+})
+
+const mockAddAlbumModel = (name: string): AddAlbumModel => ({
+    artist_id: 'any_id_red',
+    name: name,
+    year: '2000',
+    youtube: 'http://youtube.com',
+    images: [' image_1', 'image_2', 'image_3', 'image_4']
+})
+
+const mockAddAlbumRepository = (): AddAlbumRepository => {
+    class AddAlbumRepositoryStub implements AddAlbumRepository {
+        async add(album: AddAlbumModel): Promise<AlbumModel> {
+            return Promise.resolve(mockAlbumModel('a7x'))
+        }
+    }
+    return new AddAlbumRepositoryStub()
+}
+
+const mockLoadAlbumByNameRepositoryStub = (): LoadAlbumByNameRepository => {
+    class LoadAlbumByNameRepositoryStub implements LoadAlbumByNameRepository {
+        async loadByName(name: string): Promise<AlbumModel> {
+            return Promise.resolve(mockAlbumModel('a7x'))
+        }
+    }
+    return new LoadAlbumByNameRepositoryStub()
+}
+
+type SutType = {
+    sut: AddAlbumRepository,
+    addAlbumRepositoryStub: AddAlbumRepository
+    loadAlbumByNameRepositoryStub: LoadAlbumByNameRepository
+}
+
+const makeSut = (): SutType => {
+    const addAlbumRepositoryStub = mockAddAlbumRepository()
+    const loadAlbumByNameRepositoryStub = mockLoadAlbumByNameRepositoryStub()
+    const sut = new DbAddAlbum(addAlbumRepositoryStub, loadAlbumByNameRepositoryStub)
+
+    return {
+        sut,
+        addAlbumRepositoryStub,
+        loadAlbumByNameRepositoryStub
+    }
+}
+
+describe('DbAddAlbum UseCase', () => {
+    test('Should call LoadAlbumByNameRepository with correct value', async () => {
+        const { sut, loadAlbumByNameRepositoryStub } = makeSut()
+        const loadByNameSpy = jest.spyOn(loadAlbumByNameRepositoryStub, 'loadByName')
+        await sut.add(mockAddAlbumModel('a7x'))
+        expect(loadByNameSpy).toHaveBeenCalledWith('a7x')
+    })
+})
