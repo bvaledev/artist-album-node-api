97bd4fe78554d3f4f7d28e29a7213401ebd53200
Brendo Vale <brendo.vale02@gmail.com>
2021-01-10 18:48:25 -0400

    refactor: move add artist factory to its own file

--- src/main/factory/controller/artist/add-artist/add-artist-controller.ts
+++ src/main/factory/controller/artist/add-artist/add-artist-controller.ts
@@ -1,6 +1,6 @@
 import { AddArtistController } from '@/presentation/controllers/artists/add/add-artist-controller'
 import { Controller } from '@/presentation/protocols'
-import { makeDbAddArtistFactory } from '../../usecases/make-db-add-artist-factory'
+import { makeDbAddArtistFactory } from '../../../usecases/make-db-add-artist-factory'
 import { makeAddArtistValidationComposite } from './add-artist-validation'
 
 export const makeAddArtistControllerFactory = (): Controller => {
--- src/main/routes/artist-routes.ts
+++ src/main/routes/artist-routes.ts
@@ -1,6 +1,6 @@
 import { Router } from 'express'
 import { routeAdapter } from '@/main/adapter/express/express-routes-adapter'
-import { makeAddArtistControllerFactory } from '../factory/controller/artist/add-artist-controller'
+import { makeAddArtistControllerFactory } from '../factory/controller/artist/add-artist/add-artist-controller'
 
 export default (router: Router): void => {
   router.post('/artist/add', routeAdapter(makeAddArtistControllerFactory()))
