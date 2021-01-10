ec4155c199706ee8d00f02471f065d4c82f4678b
Brendo Vale <brendo.vale02@gmail.com>
2021-01-09 20:49:10 -0400

    test: ensure AddArtistController return 400 if validation return an error

--- src/presentation/controllers/artists/add/add-artist-controller.spec.ts
+++ src/presentation/controllers/artists/add/add-artist-controller.spec.ts
@@ -60,4 +60,11 @@ describe('AddArtistController', () => {
     await sut.handle(httpRequest)
     expect(validateSpy).toHaveBeenCalledWith(httpRequest.body)
   })
+
+  test('Should return 400 if validation return an error', async () => {
+    const { sut, validationStub } = makeSut()
+    jest.spyOn(validationStub, 'validate').mockReturnValueOnce(new MissingParamError('any_field'))
+    const httpResponse = await sut.handle(mockRequest())
+    expect(httpResponse).toEqual(badRequest(new MissingParamError('any_field')))
+  })
 })
